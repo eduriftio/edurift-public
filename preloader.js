db23e0786272e9ccfa6e24e1d5dd47bdad7f4148
Mikhail <145130088+eduriftio@users.noreply.github.com>
2024-01-23 17:09:22 +1000

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -9,7 +9,7 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.8);
+background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
 
